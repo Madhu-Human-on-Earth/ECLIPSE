715ca0f010d9ea504d3ad25c639305b71ec3aeef
MADHU <116476425+Madhu-Human-on-Earth@users.noreply.github.com>
2024-09-30 13:36:34 +0530

    Add files via upload

---
+++ THREEJSLEARN/src/main/java/com/jpa/test/ThreejslearnApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreejslearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreejslearnApplication.class, args);
+	}
+
+}
---
+++ THREEJSLEARN/src/main/java/com/jpa/test/model/Product.java
@@ -0,0 +1,31 @@
+package com.jpa.test.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int uid;
+	private String email;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
